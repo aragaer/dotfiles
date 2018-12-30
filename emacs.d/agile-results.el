0d39f7fb92dd3bb9f28ac23423443cd0422e1d2d
aragaer <aragaer@gmail.com>
2018-12-30 13:47:14 +0300

    agile-results templates again

--- emacs.d/agile-results.el
+++ emacs.d/agile-results.el
@@ -1,31 +1,32 @@
 (defconst *my-agile-results-file* "~/Dropbox/org/agile-results.org")
 
-(defun agile-result-goto-week ()
+(defun agile-append ()
   (setq hd (format-time-string "Week %W (%Y)" (current-time)))
   (goto-char (point-min))
   (if (re-search-forward
        (format org-complex-heading-regexp-format (regexp-quote hd))
        nil t)
       (goto-char (point-at-bol))
-    (goto-char (point-min))
-    (search-forward "*") (backward-char)
-    (org-insert-heading) (insert hd)))
+    (search-forward "*")
+    (while (org-up-heading-safe))
+    (org-insert-heading) (insert hd)
+    (backward-char)))
 
 (defun add-agile-result-templates()
   (add-to-list 'org-capture-templates '("a" "Agile results templates"))
   (add-to-list 'org-capture-templates '("am" "Monday Vision" entry (file+function
                                                                     *my-agile-results-file*
-                                                                    agile-result-goto-week)
+                                                                    agile-append)
                                         (file "~/Dropbox/org/templates/monday-vision.org")
                                         :unnarrowed t))
   (add-to-list 'org-capture-templates '("ad" "Daily Outcomes" entry (file+function
                                                                      *my-agile-results-file*
-                                                                     agile-result-goto-week)
+                                                                     agile-append)
                                         (file "~/Dropbox/org/templates/daily-outcomes.org")
                                         :unnarrowed t))
   (add-to-list 'org-capture-templates '("af" "Friday Reflection" entry (file+function
                                                                         *my-agile-results-file*
-                                                                        agile-result-goto-week)
+                                                                        agile-append)
                                         (file "~/Dropbox/org/templates/friday-reflection.org")
                                         :unnarrowed t)))
 
