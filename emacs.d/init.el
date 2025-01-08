63d47d5d166a54c444dfa1b8464983e56550091d
aragaer <aragaer@gmail.com>
2025-01-08 22:28:33 +0300

    emacs: rewrite narrow code, some cleanup

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -8,15 +8,16 @@
 
 (package-initialize)
 
-(cl-flet ((load-config (name)
-            "Load the Emacs Lisp file tangled from an Org file called NAME and
+(defun aragaer--load-config (name)
+  "Load the Emacs Lisp file tangled from an Org file called NAME and
 placed in the user emacs directory, if that tangled code is still
 actual. Otherwise, resort to `org-babel-load-file'."
-            (let* ((org-file (expand-file-name name user-emacs-directory))
-                   (elisp-file (concat (file-name-sans-extension org-file) ".el")))
-              (when (file-exists-p org-file)
-                (if (file-newer-than-file-p elisp-file org-file)
-                    (load-file elisp-file)
-                  (org-babel-load-file org-file))))))
-  (load-config "myinit.org"))
+  (let* ((org-file (expand-file-name name user-emacs-directory))
+         (elisp-file (concat (file-name-sans-extension org-file) ".el")))
+    (when (file-exists-p org-file)
+      (if (file-newer-than-file-p elisp-file org-file)
+          (load-file elisp-file)
+        (org-babel-load-file org-file)))))
+
+(aragaer--load-config "myinit.org")
 
